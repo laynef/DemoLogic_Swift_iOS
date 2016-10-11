8081654d13e6a3ae9eb8d8e484a1040faf099849
lfaler <laynefaler@gmail.com>
2016-10-11 12:41:00 -0700

    Initial commit

---
+++ DemoFullLogin/GoogleLoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleLoginViewController.swift
+//  DemoFullLogin
+//
+//  Created by Layne Faler on 6/9/16.
+//  Copyright © 2016 Layne Faler. All rights reserved.
+//
+
+import UIKit
+
+class GoogleLoginViewController: UIViewController {
+
+}
